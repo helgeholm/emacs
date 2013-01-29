727c0c764d6fc43c1dc7f9689d9b1c2230534da4
Helge Skogly Holm <helge.holm@gmail.com>
2013-01-29 22:49:41 +0100

    My EMACS let me keep it for ever and ever

---
+++ emacs.el
@@ -0,0 +1,40 @@
+(add-to-list 'load-path "~/.emacs.d/php-mode")
+(require 'php-mode)
+(add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
+
+(add-to-list 'load-path "~/.emacs.d/coffee-mode")
+(require 'coffee-mode)
+(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
+(add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
+(setq coffee-tab-width 4)
+(setq js-indent-level 2)
+
+(add-to-list 'load-path "~/.emacs.d/jade-mode")
+(require 'sws-mode)
+(require 'jade-mode)    
+(add-to-list 'auto-mode-alist '("\\.styl$" . sws-mode))
+(add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
+
+(tool-bar-mode -1)
+
+(setq-default indent-tabs-mode nil)
+(setq column-number-mode 1)
+(setq make-backup-files nil)
+(require 'color-theme)
+(eval-after-load "color-theme"
+  '(progn
+     (color-theme-initialize)
+     (color-theme-hober)))
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t)
+ '(safe-local-variable-values (quote ((js-indent-level . 4) (js-indent-level . 2)))))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
